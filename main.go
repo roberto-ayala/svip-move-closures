ad54ab171f7efe0479efe7469ee21c86c53abbe0
Roberto Ayala <dev@rayala.org>
2024-12-04 11:07:59 -0300

    several changes, add progress bar and add missing fields to checkin

--- main.go
+++ main.go
@@ -4,13 +4,17 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"os"
 	"sync"
 	"time"
 
+	"github.com/schollz/progressbar/v3"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
+	"golang.org/x/text/language"
+	"golang.org/x/text/message"
 )
 
 // Struct para el salón
@@ -19,6 +23,9 @@ type Salon struct {
 	Nombre   string `bson:"nombre,omitempty"`
 	Pais     string `bson:"pais,omitempty"`
 	Terminal int    `bson:"terminal,omitempty"`
+	// private Long tarifaIngresoSinConvenio;
+	// private List<IpEquipo> lstIpEquipo;
+	// private List<BancoSalon> bancos;
 }
 
 // Struct para el usuario
@@ -58,39 +65,85 @@ type MedioPago struct {
 	ID  string `bson:"$id"`
 }
 
+type Benefit struct {
+	Code           string `bson:"code,omitempty"`
+	Description    string `bson:"description,omitempty"`
+	Currency       string `bson:"currency,omitempty"`
+	Quantity       int32  `bson:"quantity,omitempty"`
+	Value          int32  `bson:"value,omitempty"`
+	AvailableByDay int32  `bson:"availableByDay,omitempty"`
+	Used           int32  `bson:"used,omitempty"`
+}
+
+type Reversa struct {
+	FechaReversa  time.Time `bson:"fechaReversa,omitempty"`
+	Usuario       string    `bson:"usuario,omitempty"`
+	ResponseBanco string    `bson:"responseBanco,omitempty"`
+}
+
 // Struct para el checkin
 type Checkin struct {
-	ID                             string    `bson:"_id"`
-	CantidadAcompanantes           int       `bson:"cantidadAcompanantes,omitempty"`
-	Firma                          string    `bson:"firma,omitempty"`
-	FechaIngreso                   time.Time `bson:"fechaIngreso,omitempty"`
-	Sincronizado                   bool      `bson:"sincronizado,omitempty"`
-	CodigoAutorizacionTbk          string    `bson:"codigoAutorizacionTbk,omitempty"`
-	TotalPagadoTbk                 int64     `bson:"totalPagadoTbk,omitempty"`
-	TotalVoucherConvenio           int64     `bson:"totalVoucherConvenio,omitempty"`
-	CantidadAcoPagadoTbk           int       `bson:"cantidadAcoPagadoTbk,omitempty"`
-	CantidadAcoEnConvenio          int       `bson:"cantidadAcoEnConvenio,omitempty"`
-	NumeroTarjeta                  string    `bson:"numeroTarjeta,omitempty"`
-	Turno                          string    `bson:"turno,omitempty"`
-	IngresoEspecial                bool      `bson:"ingresoEspecial,omitempty"`
-	ServicioBChile                 bool      `bson:"servicioBChile,omitempty"`
-	ReCheckin                      bool      `bson:"reCheckin,omitempty"`
-	TitularAmount                  int64     `bson:"titularAmount,omitempty"`
-	ItauAccessExchange             bool      `bson:"itauAccessExchange,omitempty"`
-	ItauAccessExchangeOffline      bool      `bson:"itauAccessExchangeOffline,omitempty"`
-	BancoChileCuposLibres          int       `bson:"bancoChileCuposLibres,omitempty"`
-	BancoChileCuposPremium         int       `bson:"bancoChileCuposPremium,omitempty"`
-	BancoChileCuposPromocionales   int       `bson:"bancoChileCuposPromocionales,omitempty"`
-	BancoChileCuposIngresoReducido int       `bson:"bancoChileCuposIngresoReducido,omitempty"`
-	MontoConvenio                  int64     `bson:"montoConvenio,omitempty"`
-	MontoSinConvenio               int64     `bson:"montoSinConvenio,omitempty"`
-	FalabellaRegister              bool      `bson:"falabellaRegister,omitempty"`
-	FalabellaRegisterOffline       bool      `bson:"falabellaRegisterOffline,omitempty"`
-	AccessMode                     string    `bson:"accessMode,omitempty"`
-	User                           User      `bson:"user,omitempty"`
-	Cliente                        Cliente   `bson:"cliente,omitempty"`
-	Banco                          Banco     `bson:"banco,omitempty"`
-	MedioPago                      MedioPago `bson:"medioPago,omitempty"`
+	ID                   string    `bson:"_id"`
+	CantidadAcompanantes int       `bson:"cantidadAcompanantes,omitempty"`
+	Firma                string    `bson:"firma,omitempty"`
+	FechaIngreso         time.Time `bson:"fechaIngreso,omitempty"`
+	Sincronizado         bool      `bson:"sincronizado,omitempty"`
+
+	IdCierreServer          string `bson:"idCierreServer,omitempty"`
+	CodigoAutorizacionBanco string `bson:"codigoAutorizacionBanco,omitempty"`
+
+	CodigoAutorizacionTbk string `bson:"codigoAutorizacionTbk,omitempty"`
+	TotalPagadoTbk        int64  `bson:"totalPagadoTbk,omitempty"`
+	TotalVoucherConvenio  int64  `bson:"totalVoucherConvenio,omitempty"`
+	CantidadAcoPagadoTbk  int    `bson:"cantidadAcoPagadoTbk,omitempty"`
+	CantidadAcoEnConvenio int    `bson:"cantidadAcoEnConvenio,omitempty"`
+	NumeroTarjeta         string `bson:"numeroTarjeta,omitempty"`
+	Turno                 string `bson:"turno,omitempty"`
+
+	IdSkyNumber         string `bson:"idSkyNumber,omitempty"`
+	NumeroReserva       string `bson:"numeroReserva,omitempty"`
+	NumeroVuelo         string `bson:"numeroVuelo,omitempty"`
+	NombreAcompanante   string `bson:"nombreAcompanante,omitempty"`
+	ApellidoAcompanante string `bson:"apellidoAcompanante,omitempty"`
+	CodigoPax           string `bson:"codigoPax,omitempty"`
+
+	IngresoEspecial        bool    `bson:"ingresoEspecial,omitempty"`
+	SincronizadoBancoChile bool    `bson:"sincronizadoBancoChile,omitempty"`
+	ResponseBancoChile     string  `bson:"responseBancoChile,omitempty"`
+	Reversa                Reversa `bson:"reversa,omitempty"`
+
+	ServicioBChile            bool  `bson:"servicioBChile,omitempty"`
+	ReCheckin                 bool  `bson:"reCheckin,omitempty"`
+	TitularAmount             int64 `bson:"titularAmount,omitempty"`
+	ItauAccessExchange        bool  `bson:"itauAccessExchange,omitempty"`
+	ItauAccessExchangeOffline bool  `bson:"itauAccessExchangeOffline,omitempty"`
+
+	Transaction                int64  `bson:"transaction,omitempty"`
+	PudahuelTransactionMessage string `bson:"pudahuelTransactionMessage,omitempty"`
+
+	// Deprecated fields
+	BancoChileCuposLibres          int `bson:"bancoChileCuposLibres,omitempty"`
+	BancoChileCuposPremium         int `bson:"bancoChileCuposPremium,omitempty"`
+	BancoChileCuposPromocionales   int `bson:"bancoChileCuposPromocionales,omitempty"`
+	BancoChileCuposIngresoReducido int `bson:"bancoChileCuposIngresoReducido,omitempty"`
+
+	MontoConvenio            int64 `bson:"montoConvenio,omitempty"`
+	MontoSinConvenio         int64 `bson:"montoSinConvenio,omitempty"`
+	FalabellaRegister        bool  `bson:"falabellaRegister,omitempty"`
+	FalabellaRegisterOffline bool  `bson:"falabellaRegisterOffline,omitempty"`
+
+	AccessMode string `bson:"accessMode,omitempty"`
+
+	Salon     Salon     `bson:"salon,omitempty"`
+	User      User      `bson:"user,omitempty"`
+	Cliente   Cliente   `bson:"cliente,omitempty"`
+	Banco     Banco     `bson:"banco,omitempty"`
+	MedioPago MedioPago `bson:"medioPago,omitempty"`
+	// CheckinRel Checkin   `bson:"checkinRel,omitempty"`
+
+	// Integrations
+	Integration string    `bson:"integration,omitempty"`
+	Benefits    []Benefit `bson:"benefits,omitempty"`
 }
 
 // Struct para el cierre
@@ -126,6 +179,8 @@ func main() {
 	cierreCollection := db.Collection("cierre")
 	checkinCollection := db.Collection("checkins")
 
+	count := countDocs(cierreCollection)
+
 	// Procesar todos los cierres
 	cursor, err := cierreCollection.Find(context.TODO(), bson.D{})
 	if err != nil {
@@ -136,6 +191,8 @@ func main() {
 	// Contador para las goroutines
 	const numGoroutines = 20
 	semaphore := make(chan struct{}, numGoroutines)
+
+	bar := progressbar.Default(count)
 
 	for cursor.Next(context.TODO()) {
 		var cierre Cierre
@@ -151,16 +208,41 @@ func main() {
 				processCierre(cierreCollection, checkinCollection, cierre)
 			}(cierre)
 		}
-
+		bar.Add(1)
 	}
 
 	wg.Wait() // Esperar que todas las goroutines finalicen
 	fmt.Println("Proceso completado")
 }
 
+func Getenv(key, defaultValue string) string {
+	value, defined := os.LookupEnv(key)
+	if !defined {
+		return defaultValue
+	}
+	return value
+}
+
+func countDocs(collectionCierre *mongo.Collection) int64 {
+	startTime := time.Now() // cronómetro
+	opts := options.Count().SetHint("_id_")
+	p := message.NewPrinter(language.Spanish)
+
+	log.Println("Contando los documentos a procesar")
+	count, err := collectionCierre.CountDocuments(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	elapsedTime := time.Since(startTime)
+	log.Printf("Tiempo total de contéo: %s. Total de documentos a procesar: %s\n",
+		elapsedTime, p.Sprint(count))
+	return count
+}
+
 // Función para procesar cada cierre
 func processCierre(cierreCollection, checkinCollection *mongo.Collection, cierre Cierre) {
-	log.Printf("Procesando cierre: %s", cierre.ID)
+	// log.Printf("Procesando cierre: %s", cierre.ID)
 
 	var checkinIDs []string
 
@@ -198,6 +280,6 @@ func processCierre(cierreCollection, checkinCollection *mongo.Collection, cierre
 	if err != nil {
 		log.Printf("Error actualizando cierre %s: %v", cierre.ID, err)
 	} else {
-		log.Printf("Cierre %s actualizado con referencias a los checkins", cierre.ID)
+		// log.Printf("Cierre %s actualizado con referencias a los checkins", cierre.ID)
 	}
 }
